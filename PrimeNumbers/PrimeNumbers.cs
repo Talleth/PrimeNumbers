a7b20767b345b426840ac3ce908391ad7cb98969
Talleth <49004551+Talleth@users.noreply.github.com>
2019-11-10 08:08:25 -0800

    Implemented methods

---
+++ PrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    public class PrimeNumbers
+    {
+        public static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+
+            for (int i=2; i<number; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public static List<int> CalculateFromRange(int start, int end)
+        {
+            List<int> result = new List<int>();
+
+            if (start <= 1)
+                start = 2;
+
+            for (int i=start; i<=end; i++)
+            {
+                if (PrimeNumbers.IsPrimeNumber(i))
+                    result.Add(i);
+            }
+
+            return result;
+        }
+    }
+}
--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace PrimeNumbers
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] arguments)
         {
+            // Example 1:
+            // From 1 - 20
+            // Result: 2, 3, 5, 7, 11, 13, 17, 19
+            Console.WriteLine("1 - 20");
+            Console.WriteLine(string.Join(",", PrimeNumbers.CalculateFromRange(1, 20)));
+            Console.WriteLine();
+
+            // Example 1:
+            // From 10 - 50
+            // Result: 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47
+            Console.WriteLine("10 - 50");
+            Console.WriteLine(string.Join(",", PrimeNumbers.CalculateFromRange(10, 50)));
+            Console.WriteLine();
+
+            // Example 1:
+            // From 1000 - 1050
+            // Result: 1,009, 1,013, 1,019, 1,021, 1,031, 1,033, 1,039, 1,049
+            Console.WriteLine("1000 - 1050");
+            Console.WriteLine(string.Join(",", PrimeNumbers.CalculateFromRange(1000, 1050)));
+            Console.WriteLine();
         }
     }
 }
